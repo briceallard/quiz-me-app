e302077fb348ed400afd0f0da1f3e2bfc9216763
Brice Allard <briceallard@gmail.com>
2019-11-02 17:07:28 -0500

    update

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -6,14 +6,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       top: true,
       child: Scaffold(
-        body: Container(),
+        body: Container(
+          child: Text('Home page'),
+        ),
       ),
     );
   }
 }
+
+/*
+
+Track work progress, approximate completion time and arrival time
+automatic notification for tracking progress (on the way, complete)
+Quote customers prices based on submitted pictures
+Check schedule for work order progression
+
+** Services offered by business **
+Landscaping
+Sprinklers
+Spraying chemicals
+Mowing
+Bids
+Snow removal
+
+** Expected costs **
+
+www.absolutelawncareonline.com
+5,000+
+recurring monthly maintenance fee's ??? Probable
+
+*/
